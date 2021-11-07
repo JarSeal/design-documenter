8547f0f2c2004f57f4e1e31a95a668a73b1da3d8
JarSeal <kai.forsman@gmail.com>
2021-11-07 20:35:27 +0800

    Eslint fixes

--- back/.eslintrc.js
+++ back/.eslintrc.js
@@ -1,34 +1,34 @@
 module.exports = {
-  'env': {
-    'commonjs': true,
-    'es2021': true,
-    'node': true,
-    'jest': true,
-  },
-  'extends': 'eslint:recommended',
-  'globals': {
-    'Atomics': 'readonly',
-    'SharedArrayBuffer': 'readonly'
-  },
-  'parserOptions': {
-    'ecmaVersion': 2018
-  },
-  'rules': {
-    'indent': [
-      'error',
-      2
-    ],
-    // 'linebreak-style': [
-    //   'error',
-    //   'unix'
-    // ],
-    'quotes': [
-      'error',
-      'single'
-    ],
-    'semi': [
-      'error',
-      'always'
-    ]
-  }
+    'env': {
+        'commonjs': true,
+        'es2021': true,
+        'node': true,
+        'jest': true,
+    },
+    'extends': 'eslint:recommended',
+    'globals': {
+        'Atomics': 'readonly',
+        'SharedArrayBuffer': 'readonly'
+    },
+    'parserOptions': {
+        'ecmaVersion': 2018
+    },
+    'rules': {
+        'indent': [
+            'error',
+            4
+        ],
+        // 'linebreak-style': [
+        //   'error',
+        //   'unix'
+        // ],
+        'quotes': [
+            'error',
+            'single'
+        ],
+        'semi': [
+            'error',
+            'always'
+        ]
+    }
 };
--- back/controllers/users.js
+++ back/controllers/users.js
@@ -4,44 +4,44 @@ const User = require('./../models/user');
 
 // Get all users
 usersRouter.get('/', async (request, response) => {
-  const result = await User.find({}).populate('userGroups', {
-    name: 1, id: 1
-  });
-  response.json(result);
+    const result = await User.find({}).populate('userGroups', {
+        name: 1, id: 1
+    });
+    response.json(result);
 });
 
 // Register user
 usersRouter.post('/', async (request, response) => {
-  const body = request.body;
+    const body = request.body;
 
-  if(!body.username || body.username.length < 5) {
-    response.status(400).json({ error: 'username too short or missing' });
-    return;
-  }
+    if(!body.username || body.username.length < 5) {
+        response.status(400).json({ error: 'username too short or missing' });
+        return;
+    }
 
-  if(!body.email || body.email.length < 5) {
-    response.status(400).json({ error: 'email too short or missing' });
-    return;
-  }
+    if(!body.email || body.email.length < 5) {
+        response.status(400).json({ error: 'email too short or missing' });
+        return;
+    }
   
-  if(!body.password || body.password.length < 6) {
-    response.status(400).json({ error: 'password too short or missing' });
-    return;
-  }
+    if(!body.password || body.password.length < 6) {
+        response.status(400).json({ error: 'password too short or missing' });
+        return;
+    }
 
-  const saltRounds = 10;
-  const passwordHash = await bcrypt.hash(body.password, saltRounds);
+    const saltRounds = 10;
+    const passwordHash = await bcrypt.hash(body.password, saltRounds);
 
-  const user = new User({
-    username: body.username,
-    email: body.email,
-    name: body.name,
-    passwordHash,
-  });
+    const user = new User({
+        username: body.username,
+        email: body.email,
+        name: body.name,
+        passwordHash,
+    });
   
-  const savedUser = await user.save();
+    const savedUser = await user.save();
 
-  response.json(savedUser);
+    response.json(savedUser);
 });
 
 module.exports = usersRouter;
