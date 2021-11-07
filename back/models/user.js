f7a25ce2cb3110b27062640c7c255a5d9dd31731
JarSeal <kai.forsman@gmail.com>
2021-11-07 20:43:31 +0800

    Eslint fixes

--- back/models/user.js
+++ back/models/user.js
@@ -6,57 +6,57 @@ mongoose.set('useFindAndModify', false);
 mongoose.set('useCreateIndex', true);
 
 const userSchema = mongoose.Schema({
-  username: {
-    type: String,
-    required: true,
-    unique: true,
-    minlength: 5
-  },
-  name: String,
-  email: {
-    type: String,
-    required: true,
-    minlength: 5
-  },
-  passwordHash: String,
-  userGroups: [
-    {
-      type: mongoose.Schema.Types.ObjectId,
-      ref: 'UserGroup'
+    username: {
+        type: String,
+        required: true,
+        unique: true,
+        minlength: 5
+    },
+    name: String,
+    email: {
+        type: String,
+        required: true,
+        minlength: 5
+    },
+    passwordHash: String,
+    userGroups: [
+        {
+            type: mongoose.Schema.Types.ObjectId,
+            ref: 'UserGroup'
+        }
+    ],
+    rights: {
+        universe: {
+            canCreate: Boolean,
+            canEdit: Boolean,
+            canDelete: Boolean,
+        },
+        structure: {
+            canCreate: Boolean,
+            canEdit: Boolean,
+            canDelete: Boolean,
+        },
+        beacon: {
+            canCreate: Boolean,
+            canEdit: Boolean,
+            canDelete: Boolean,
+        },
+        dataset: {
+            canCreate: Boolean,
+            canEdit: Boolean,
+            canDelete: Boolean,
+        },
     }
-  ],
-  rights: {
-    universe: {
-      canCreate: Boolean,
-      canEdit: Boolean,
-      canDelete: Boolean,
-    },
-    structure: {
-      canCreate: Boolean,
-      canEdit: Boolean,
-      canDelete: Boolean,
-    },
-    beacon: {
-      canCreate: Boolean,
-      canEdit: Boolean,
-      canDelete: Boolean,
-    },
-    dataset: {
-      canCreate: Boolean,
-      canEdit: Boolean,
-      canDelete: Boolean,
-    },
-  }
 });
 
 userSchema.plugin(uniqueValidator);
 userSchema.set('toJSON', {
-  transform: (document, returnedObject) => {
-    returnedObject.id = returnedObject._id.toString();
-    delete returnedObject._id;
-    delete returnedObject.__v;
-    delete returnedObject.passwordHash;
-  }
+    transform: (document, returnedObject) => {
+        returnedObject.id = returnedObject._id.toString();
+        delete returnedObject._id;
+        delete returnedObject.__v;
+        delete returnedObject.passwordHash;
+    }
 });
 
 const User = mongoose.model('User', userSchema, 'users');
--- back/models/userGroup.js
+++ back/models/userGroup.js
@@ -5,36 +5,36 @@ mongoose.set('useFindAndModify', false);
 mongoose.set('useCreateIndex', true);
 
 const userGroupSchema = mongoose.Schema({
-  username: {
-    type: String,
-    required: true,
-    unique: true,
-    minlength: 5,
-  },
-  level: {
-    type: Number,
-    required: true,
-  },
-  creator: {
-    id: mongoose.Schema.Types.ObjectId,
-    date: Date,
-  },
-  users: [
-    {
-      type: mongoose.Schema.Types.ObjectId,
-      ref: 'User',
-      rights: [],
-    }
-  ],
+    username: {
+        type: String,
+        required: true,
+        unique: true,
+        minlength: 5,
+    },
+    level: {
+        type: Number,
+        required: true,
+    },
+    creator: {
+        id: mongoose.Schema.Types.ObjectId,
+        date: Date,
+    },
+    users: [
+        {
+            type: mongoose.Schema.Types.ObjectId,
+            ref: 'User',
+            rights: [],
+        }
+    ],
 });
 
 userGroupSchema.plugin(uniqueValidator);
 userGroupSchema.set('toJSON', {
-  transform: (document, returnedObject) => {
-    returnedObject.id = returnedObject._id.toString();
-    delete returnedObject._id;
-    delete returnedObject.__v;
-  }
+    transform: (document, returnedObject) => {
+        returnedObject.id = returnedObject._id.toString();
+        delete returnedObject._id;
+        delete returnedObject.__v;
+    }
 });
 
 const UserGroup = mongoose.model('UserGroup', userGroupSchema, 'usergroups');
