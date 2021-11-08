2c7396852c9e47d8565be1de2065ff2e8bdeb2e0
JarSeal <kai.forsman@gmail.com>
2021-11-08 11:21:30 +0800

    Form server side validation

--- back/controllers/forms.js
+++ back/controllers/forms.js
@@ -1,10 +1,19 @@
 const formsRouter = require('express').Router();
 const Form = require('./../models/form');
+const { validateField, validateKeys } = require('./forms/formValidator');
 const newUserFormData = require('./forms/newUserFormData');
 
 // Get all forms
 formsRouter.get('/', async (request, response) => {
-    const result = await Form.find({});
+    let result = await Form.find({});
+    if(result.length < presetForms.length) {
+        // Create missing forms
+        for(let i=0; i<presetForms.length; i++) {
+            result = await Form.findOne({ formId: presetForms[i] });
+            if(!result) await _createPresetForm(presetForms[i]);
+        }
+        result = await Form.find({});
+    }
     response.json(result);
 });
 
@@ -27,15 +36,45 @@ formsRouter.get('/:id', async (request, response) => {
 // Create a new form
 formsRouter.post('/', async (request, response) => {
     request, response;
+    // TODO
 });
 
 // Send a filled form
 formsRouter.post('/filled', async (request, response) => {
-    request, response;
-    console.log('HERE');
+    const body = request.body;
+    const formData = await Form.findOne({ formId: body.id });
+    if(!formData || !formData.form) {
+        response.status(404).json({ msg: 'Could not find form (' + body.id + '),' });
+        return;
+    }
+
+    const keys = Object.keys(body);
+    const keysFound = validateKeys(formData.form, keys);
+    if(!keysFound) {
+        response.status(400).json({ msg: 'Bad request. Payload missing or incomplete.' });
+        return;
+    }
+
+    const errors = {};
+    for(let i=0; i<keys.length; i++) {
+        let error = validateField(formData.form, keys[i], body[keys[i]]);
+        if(error) errors[keys[i]] = error;
+    }
+    const errorKeys = Object.keys(errors);
+    if(errorKeys.length) {
+        response.status(400).json({ msg: 'Bad request. Validation errors.', errors });
+        return;
+    }
+
+    if(body.id === 'new-user-form') {
+        // Special case for registering a new user
+
+    }
     response.json({ msg: 'filledForm' });
+    // TODO
 });
 
+const presetForms = ['new-user-form'];
 const _createPresetForm = async (id) => {
     let newForm, form;
     if(id === 'new-user-form') {
--- back/controllers/forms/formValidator.js
+++ back/controllers/forms/formValidator.js
@@ -1,5 +1,65 @@
-const validateField = (field, value) => {
-    field, value;
+const validateField = (form, key, value) => {
+    if(key === 'id') return null;
+    
+    const fieldsets = form.fieldsets;
+    for(let i=0; i<fieldsets.length; i++) {
+        const fieldset = fieldsets[i];
+        for(let j=0; j<fieldset.fields.length; j++) {
+            const field = fieldset.fields[j];
+            if(field.id === key) {
+                switch(field.type) {
+                case 'textinput':
+                    return textInput(field, value);
+                case 'checkbox':
+                    return checkbox(field, value);
+                case 'dropdown':
+                    return dropdown(field, value);
+                default:
+                    return checkAllowedFieldTypes(field.type);
+                }
+            }
+        }
+    }
 };
 
-module.exports = { validateField };
+const textInput = (field, value) => {
+    if(field.required && (!value || value.trim() === '')) return 'Required.';
+    
+    return null;
+};
+
+const checkbox = (field, value) => {
+    if(field.required && (!value || value === false)) return 'Required.';
+    return null;
+};
+
+const dropdown = (field, value) => {
+    if(field.required && (!value || value.trim() === '')) return 'Required.';
+    return null;
+};
+
+const checkAllowedFieldTypes = (type) => {
+    if(type === 'divider' || type === 'subheading') {
+        return null;
+    }
+    return 'Field type not found.';
+};
+
+const validateKeys = (form, keys) => {
+    console.log('VALIDATA');
+    const fieldsets = form.fieldsets;
+    let keysFound = 1; // Id is counted as one
+    for(let i=0; i<fieldsets.length; i++) {
+        const fieldset = fieldsets[i];
+        for(let j=0; j<fieldset.fields.length; j++) {
+            const field = fieldset.fields[j];
+            for(let k=0; k<keys.length; k++) {
+                if(keys[k] === field.id) keysFound++;
+            }
+        }
+    }
+    console.log(keysFound, keys.length);
+    return keysFound === keys.length;
+};
+
+module.exports = { validateField, validateKeys };
