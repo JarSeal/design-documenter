b867d08b5c563c79dfd01047d925a0bf8f7f1810
JarSeal <kai.forsman@gmail.com>
2021-11-10 11:55:34 +0800

    Universe documentation

---
+++ documentation/universe.js
@@ -0,0 +1,7 @@
+// Documentation example for a Universe
+
+const universe = {
+    title: 'My Project Title', // String [required]
+    universeId: 'my-project-title', // String [required, unique, regex='^[a-zA-Z0-9\-_]+$']
+    description: 'Some project description...', // String
+};
