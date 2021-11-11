4b6127c91fe66b6ac325e5bd782ab641514c5d10
JarSeal <kai.forsman@gmail.com>
2021-11-11 08:28:47 +0800

    New universe form

---
+++ shared/formData/newUniverseFormData.js
@@ -0,0 +1,58 @@
+const CONFIG = require('./../../CONFIG').USER;
+
+// Create a new universe:
+const newUniverseFormData = {
+    id: 'new-universe-form',
+    formTitleId: 'create_new_universe',
+    onErrorsMsgId: 'form_has_errors',
+    afterSubmitMsgId: 'new_universe_created',
+    afterSubmitShowOnlyMsg: true,
+    submitButton: {
+        id: 'submit-new-universe-button',
+        labelId: 'create_new_universe_button',
+    },
+    submitFields: [],
+    fieldsets: [
+        {
+            id: 'main-fieldset-new-uni',
+            fields: [
+                { type: 'divider' },
+                {
+                    // TITLE / NAME
+                    type: 'textinput',
+                    id: 'new-uni-title',
+                    labelId: 'name',
+                    required: true,
+                    minLength: 1,
+                    maxLength: 40,
+                    regex: '[a-zA-ZåöäñüéèêâîôûčßàìòùóçęįųķļņģëïõžšæøėēūāīÅÖÄÑÜÉÈÊÂÎÔÛČẞÀÌÒÙÓÇĘĮŲĶĻŅĢËÏÕŽŠÆØĖĒŪĀĪ]+$', // Current langs: finnish, english, swedish, norwegian, danish, german, french, spanish, italian, estonian, latvian, lithuanian
+                    regexErrorMsgId: 'field_has_invalid_characters',
+                },
+                { type: 'divider' },
+                {
+                    // UNIVERSE ID
+                    type: 'textinput',
+                    id: 'new-uni-id',
+                    labelId: 'new-universe-id',
+                    required: true,
+                    minLength: 1,
+                    maxLength: 40,
+                    regex: '^[a-zA-Z0-9-_]+$',
+                    regexErrorMsgId: 'field_has_invalid_characters',
+                },
+                { type: 'divider' },
+                {
+                    // DESCRIPTION
+                    type: 'textinput',
+                    id: 'new-uni-description',
+                    labelId: 'email',
+                    maxLength: 500,
+                    email: true,
+                },
+                { type: 'divider' },
+            ],
+        },
+    ],
+};
+
+module.exports = newUniverseFormData;
