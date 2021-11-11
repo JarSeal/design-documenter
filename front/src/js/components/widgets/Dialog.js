130fb5d6071c33cf1683be74df506ef26662cb3e
JarSeal <kai.forsman@gmail.com>
2021-11-11 10:53:10 +0800

    Button and dialog styles

--- front/src/js/components/widgets/Dialog.js
+++ front/src/js/components/widgets/Dialog.js
@@ -21,13 +21,14 @@ class Dialog extends Component {
         })));
         this.dialogCompos.push(this.addChild(new Button({
             id: this.id + '-close-button',
-            text: 'close',
+            class: 'close-button',
             attach: this.id + '-box-wrapper',
             attributes: { title: getText('close_dialog') },
-            click: this.closeDialogClick,
+            click: this._closeDialogClick,
         })));
         this.dialogCompos.push(this.addChild(new Component({
             id: this.id + '-inner-box',
+            class: 'inner-box',
             attach: this.id + '-box-wrapper',
         })));
     }
@@ -36,7 +37,7 @@ class Dialog extends Component {
         this.addListener({
             id: this.id + '-background-click',
             type: 'click',
-            fn: this.closeDialogClick,
+            fn: this._closeDialogClick,
         });
     }
 
@@ -44,7 +45,11 @@ class Dialog extends Component {
         for(let i=0; i<this.dialogCompos.length; i++) {
             this.dialogCompos[i].draw();
         }
-        if(this.dialogTitle) this.dialogTitle.draw();
+        if(this.dialogTitle) {
+            this.dialogTitle.draw();
+            this.elem.classList.add('has-title');
+            this._setTopPadding();
+        }
         if(this.compoToShow) this.compoToShow.draw();
         if(data.appear) {
             setTimeout(() => {
@@ -59,6 +64,7 @@ class Dialog extends Component {
         if(dialogData.title) {
             this.dialogTitle = this.addChild(new Component({
                 id: this.id + '-main-title',
+                class: 'main-title',
                 tag: 'h3',
                 text: dialogData.title,
                 attach: this.id + '-inner-box',
@@ -77,11 +83,17 @@ class Dialog extends Component {
         }, this.transitionTime);
     }
 
-    closeDialogClick = (e) => {
+    _closeDialogClick = (e) => {
         const targetId = e.target.id;
         if(targetId === this.id || targetId === this.id+'-close-button') {
             this.disappear();
         }
+    }
+
+    _setTopPadding = () => {
+        const titleElem = this.elem.querySelector('#'+this.id + '-main-title');
+        const boxElem = this.elem.querySelector('#'+this.id + '-box-wrapper');
+        boxElem.style.paddingTop = (titleElem.offsetHeight / 10) + 'rem';
     }
 }
 
