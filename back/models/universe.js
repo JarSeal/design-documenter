f6e6592027f813ef133087a32ceccd2a5e872a9c
JarSeal <kai.forsman@gmail.com>
2021-11-13 16:32:32 +0800

    Add user ref to edited

--- back/models/universe.js
+++ back/models/universe.js
@@ -1,6 +1,5 @@
 const mongoose = require('mongoose');
 const uniqueValidator = require('mongoose-unique-validator');
-// const User = require('./user');
 
 mongoose.set('useFindAndModify', false);
 mongoose.set('useCreateIndex', true);
@@ -26,7 +25,10 @@ const universeSchema = mongoose.Schema({
     },
     edited: [
         {
-            by: String,
+            by: {
+                type: mongoose.Schema.Types.ObjectId,
+                ref: 'User',
+            },
             date: Date,
         }
     ],
