0f8764ac1f8e9bee365fadc4decce971f328c26f
JarSeal <kai.forsman@gmail.com>
2021-11-25 13:47:13 +0800

    Read users api definition

--- shared/formData/formData.js
+++ shared/formData/formData.js
@@ -1,11 +1,13 @@
 const loginBeaconFormData = require('./loginBeaconFormData');
 const newUserFormData = require('./newUserFormData');
 const newUniverseFormData = require('./newUniverseFormData');
+const readUsersFormData = require('./readUsersFormData');
 
 const formData = [
     loginBeaconFormData,
     newUserFormData,
     newUniverseFormData,
+    readUsersFormData,
 ];
 
 module.exports = formData;
---
+++ shared/formData/readUsersFormData.js
@@ -0,0 +1,15 @@
+
+const readUsersFormData = {
+    formId: 'read-users',
+    path: '/api/users',
+    method: 'GET',
+    type: 'readapi',
+    useRightsLevel: 8,
+    useRightsUsers: [],
+    useRightsGroups: [],
+    editorRightsLevel: 8,
+    editorRightsUsers: [],
+    editorRightsGroups: [],
+};
+
+module.exports = readUsersFormData;
