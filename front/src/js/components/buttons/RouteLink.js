380db6524158511380720196f70b92b107574b2e
JarSeal <kai.forsman@gmail.com>
2021-11-25 09:54:07 +0800

    Documentation, isCurrent check and bypassCurrent

--- front/src/js/components/buttons/RouteLink.js
+++ front/src/js/components/buttons/RouteLink.js
@@ -1,8 +1,12 @@
 import Component from "../../LIGHTER/Component";
 
+// Attributes:
+// - link = String (Router link/path)
+// - bypassCurrentCheck = Boolean (change route even if the target route is the same as the current one)
 class RouteLink extends Component {
     constructor(data) {
         super(data);
+        this.isCurrent = false;
         this.template = `<button></button>`;
     }
 
@@ -17,14 +21,17 @@ class RouteLink extends Component {
     paint = (data) => {
         if(this.Router.isCurrent(data.link)) {
             this.elem.classList.add('current');
+            this.isCurrent = true;
         } else {
             this.elem.classList.remove('current');
+            this.isCurrent = false;
         }
     }
 
     click = (e) => {
         e.preventDefault();
-        this.Router.changeRoute(this.data.link);
+        if(this.isCurrent && !this.data.bypassCurrentCheck) return;
+        this.Router.changeRoute(this.data.link, true);
     }
 }
 
