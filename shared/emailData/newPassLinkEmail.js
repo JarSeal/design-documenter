c55aa435c29cbfbab6125f32fa92fd9514a5df0d
JarSeal <kai.forsman@gmail.com>
2022-03-26 22:54:49 +0100

    Add emailComponents, linkButton

--- shared/emailData/newPassLinkEmail.js
+++ shared/emailData/newPassLinkEmail.js
@@ -1,3 +1,5 @@
+const linkButton = require('./emailComponents/linkButton');
+
 const newPassLinkEmail = {
     emailId: 'new-pass-link-email',
     fromName: 'Beacon JS',
@@ -9,9 +11,7 @@ Your Link is Ready, $[username]
 
 You requested a link to reset your password. Here you go:
 
-<a href="$[newPassWTokenUrl]" target="_blank" style="padding: 8px 32px 7px; border: 1px solid #555;border-radius: 8px;font-family: Helvetica, Arial, sans-serif;font-size: 14px; color: #ffffff;text-decoration: none;font-weight:bold;display: inline-block;background: #333;">
-    Reset my password
-</a>
+${linkButton('Reset my password', '$[newPassWTokenUrl]')}
 
 Link: $[newPassWTokenUrl]
 
--- shared/emailData/verifyAccountEmail.js
+++ shared/emailData/verifyAccountEmail.js
@@ -1,3 +1,5 @@
+const linkButton = require('./emailComponents/linkButton');
+
 const verifyAccountEmail = {
     emailId: 'verify-account-email',
     fromName: 'Beacon JS',
@@ -9,9 +11,7 @@ Almost there, $[username], please verify your E-mail
 
 Please verify your account's E-mail by clicking this link:
 
-<a href="$[verifyEmailTokenUrl]" target="_blank" style="padding: 8px 32px 7px; border: 1px solid #555;border-radius: 8px;font-family: Helvetica, Arial, sans-serif;font-size: 14px; color: #ffffff;text-decoration: none;font-weight:bold;display: inline-block;background: #333;">
-    Verify
-</a>
+${linkButton('Verify', '$[verifyEmailTokenUrl]')}
 
 Link: $[verifyEmailTokenUrl]
 
